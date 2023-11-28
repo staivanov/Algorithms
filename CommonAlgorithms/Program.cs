ad4ef3dea46c4e208fac2c189350638327a5784f
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2023-11-28 15:13:54 +0200

    New class library SortingAlgorithms. SelectionSort was imlpemented.

---
+++ CommonAlgorithms/Program.cs
@@ -0,0 +1,17 @@
+﻿using SortingAlgorithms.ComparisonBased;
+
+namespace CommonAlgorithms
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int[] numbs = { 5, 3, 7, 8, 1 };
+          
+
+
+
+
+        }
+    }
+}
---
+++ SortingAlgorithms/ComparisonBased/SelectionSort.cs
@@ -0,0 +1,35 @@
+﻿namespace SortingAlgorithms.ComparisonBased
+{   //O(n^2)
+    public static class SelectionSort
+    {
+        public static int[] Sort(int[] numbers)
+        {
+            int numbersLen = numbers.Length,
+                startIndex = 0;
+
+            while (startIndex < numbersLen - 1)
+            {
+                int position = startIndex,
+                    innerStartIndex = startIndex + 1;
+
+                while (innerStartIndex < numbersLen)
+                {
+                    if (numbers[innerStartIndex] < numbers[position])
+                    {
+                        position = innerStartIndex;
+                    }
+
+                    innerStartIndex++;
+                }
+
+                int temp = numbers[position];
+                numbers[position] = numbers[startIndex];
+                numbers[startIndex] = temp;
+                
+                startIndex++;
+            }
+
+            return numbers;
+        }
+    }
+}
