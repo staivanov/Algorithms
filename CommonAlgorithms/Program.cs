faf9a099932eb9cd6bbc1ee5b73441a43b7eb90f
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2023-12-01 07:57:06 +0200

    BubbleSort was implemented.

--- CommonAlgorithms/Program.cs
+++ CommonAlgorithms/Program.cs
@@ -6,8 +6,14 @@ namespace CommonAlgorithms
     {
         static void Main()
         {
-            int[] numbs = { 5, 3, 7, 8, 1 };
+            int[] numbs = { 5, 3, 7, 4, 1 };
 
+            int[] sortedNumbers = BubbleSort.Sort(numbs);
+
+            foreach (var number in sortedNumbers)
+            {
+                Console.WriteLine(number);
+            }
         }
     }
 }
---
+++ SortingAlgorithms/ComparisonBased/BubbleSort.cs
@@ -0,0 +1,32 @@
+﻿namespace SortingAlgorithms.ComparisonBased
+{
+    public static class BubbleSort
+    {   //O(n^2)
+        public static int[] Sort(int[] numbers)
+        {
+            int numbersLen = numbers.Length,
+                rounds = numbersLen - 1;
+
+            while (rounds >= 0)
+            {
+                int counter = 0;
+
+                while (rounds > counter)
+                {
+                    if (numbers[counter] > numbers[counter + 1])
+                    {
+                        int temp = numbers[counter];
+                        numbers[counter] = numbers[counter + 1];
+                        numbers[counter + 1] = temp;
+                    }
+
+                    counter++;
+                }
+
+                rounds--;
+            }
+
+            return numbers;
+        }
+    }
+}
