c232c4740d037ae7ef92c4caa82ae0b7d2e154b1
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2023-12-05 22:29:34 +0200

    Linearh search algorithm was implemented.

--- CommonAlgorithms/Program.cs
+++ CommonAlgorithms/Program.cs
@@ -8,7 +8,6 @@ namespace CommonAlgorithms
             int[] numbs = { 101, 14, 65, 5, 3, 7, 4, 1, 99, 99 };
             int seekingNumber = 5,
                 indexOfSeekingNumber;
-
             indexOfSeekingNumber = LinearSearch.Search(numbs, seekingNumber);
 
             Console.WriteLine("The index of seeking number is: " + indexOfSeekingNumber);
