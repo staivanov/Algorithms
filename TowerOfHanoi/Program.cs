9c6ad6d37c1bd2a6fd1afd96532a5730b721c7bc
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-05-13 12:13:37 +0300

    TowerOfHanoi problem was implemented.

---
+++ TowerOfHanoi/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+
+namespace TowerOfHanoi
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Hanoi(3, 'A', 'B', 'C');
+        }
+
+        private static void Hanoi(int n, char from, char middle,
+            char destination)
+        {
+            if (n == 1)
+            {
+                WriteLine($"Moving disk {n} from {from} to {destination}");
+                return;
+            }
+
+            Hanoi(n - 1, from, destination, middle);
+            WriteLine($"Moving disk {n} from {from} to {destination}");
+            Hanoi(n - 1, middle, from, destination);
+        }
+    }
+}
